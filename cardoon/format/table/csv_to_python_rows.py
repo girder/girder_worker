750f958196d8c38aedbf7f43b7cff6adb3ba4f9d
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-03-05 13:58:13 -0500

    Add csv numeric conversion

--- cardoon/format/table/csv_to_python_rows.py
+++ cardoon/format/table/csv_to_python_rows.py
@@ -1,2 +1,13 @@
 import csv, StringIO
 output = [d for d in csv.DictReader(StringIO.StringIO(input))]
+
+# Attempt numeric conversion
+for row in output:
+    for col in row:
+        try:
+            row[col] = int(row[col])
+        except:
+            try:
+                row[col] = float(row[col])
+            except:
+                pass
