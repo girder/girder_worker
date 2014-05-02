96de728c448c078e1492fc1ac9193e5957786d63
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-05-01 23:39:01 -0400

    Initial version of travis integration

---
+++ tests/PythonTests.cmake
@@ -0,0 +1,54 @@
+include(CMakeParseArguments)
+
+set(py_coverage_rc "${PROJECT_BINARY_DIR}/tests/romanesco.coveragerc")
+set(pep8_config "${PROJECT_SOURCE_DIR}/tests/pep8.cfg")
+set(coverage_html_dir "${PROJECT_SOURCE_DIR}/docs/_build/html/py_coverage")
+
+if(PYTHON_BRANCH_COVERAGE)
+  set(_py_branch_cov True)
+else()
+  set(_py_branch_cov False)
+endif()
+
+configure_file(
+  "${PROJECT_SOURCE_DIR}/tests/romanesco.coveragerc.in"
+  "${py_coverage_rc}"
+  @ONLY
+)
+
+function(add_python_style_test name input)
+  add_test(
+    NAME ${name}
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+    COMMAND "${PEP8_EXECUTABLE}" "--config=${pep8_config}" "${input}"
+  )
+endfunction()
+
+function(add_python_test case)
+  set(name "${case}")
+
+  set(_multival_args "")
+  cmake_parse_arguments(fn "${_options}" "${_args}" "${_multival_args}" ${ARGN})
+
+  set(module tests.${case}_test)
+  
+  if(PYTHON_COVERAGE)
+    add_test(
+      NAME ${name}
+      WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+      COMMAND "${PYTHON_COVERAGE_EXECUTABLE}" run -p --append "--rcfile=${py_coverage_rc}"
+              "--source=romanesco" -m unittest -v ${module}
+    )
+  else()
+    add_test(
+      NAME ${name}
+      WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+      COMMAND "${PYTHON_EXECUTABLE}" -m unittest -v ${module}
+    )
+  endif()
+
+  if(PYTHON_COVERAGE)
+    set_property(TEST ${name} APPEND PROPERTY DEPENDS py_coverage_reset)
+    set_property(TEST py_coverage_combine APPEND PROPERTY DEPENDS ${name})
+  endif()
+endfunction()
