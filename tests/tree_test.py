305a11f892308cedf252626ab3b8a73dc46e834b
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-09-04 12:44:43 -0400

    Removing old code

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -1,5 +1,4 @@
 import bson
-import json
 import romanesco
 import unittest
 
@@ -65,17 +64,6 @@ END;"""
             outputs={"b": {"format": "nested"}}
         )
         self.assertEqual(outputs["b"]["format"], "nested")
-        expected = json.loads(
-            '{"edge_fields": ["weight"], "node_fields": '
-            '["node name", "node weight"], "node_data": '
-            '{"node name": "", "node weight": 0.0}, "children": '
-            '[{"node_data": {"node name": "", "node weight": 2.0}, '
-            '"edge_data": {"weight": 2.0}, "children": [{"node_data": '
-            '{"node name": "ahli", "node weight": 2.0}, "edge_data": '
-            '{"weight": 0.0}}, {"node_data": {"node name": "allogus", '
-            '"node weight": 3.0}, "edge_data": {"weight": 1.0}}]}, '
-            '{"node_data": {"node name": "rubribarbus", "node weight": 3.0}, '
-            '"edge_data": {"weight": 3.0}}]}')
 
         expected = {
             "edge_fields": ["weight"],
