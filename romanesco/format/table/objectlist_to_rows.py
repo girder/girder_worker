fe57c17176979cba88f36c1df425cc85d78fc8f1
Jeffrey Baumes <jeff.baumes@kitware.com>
2015-01-27 07:44:29 -0500

    Allow nested values in objectlist

--- romanesco/format/table/objectlist_to_rows.py
+++ romanesco/format/table/objectlist_to_rows.py
@@ -2,11 +2,23 @@ import collections
 
 # Attempt to keep column ordering if objects happen to have ordered keys
 field_map = collections.OrderedDict()
+rows = []
+
+def subkeys(path, obj, row):
+    if isinstance(obj, dict):
+        for k in obj:
+            if isinstance(k, (str, unicode)):
+                subkeys(path + [k], obj[k], row)
+    elif len(path) > 0:
+        field = ".".join(path)
+        field_map[field] = True
+        row[field] = obj
+
 for obj in input:
-    for key in obj:
-        if (isinstance(key, (str, unicode))):
-            field_map[key] = True
+    row = {}
+    subkeys([], obj, row)
+    rows.append(row)
 
 fields = [key for key in field_map]
 
-output = {"fields": fields, "rows": input}
+output = {"fields": fields, "rows": rows}
--- romanesco/format/table/rows_to_objectlist.py
+++ romanesco/format/table/rows_to_objectlist.py
@@ -1,1 +1,19 @@
-output = input["rows"]
+
+def set_nested(obj, path, v):
+    key = path.pop()
+
+    if len(path) == 0:
+        obj[key] = v
+        return
+
+    obj[key] = {}
+    set_nested(obj[key], path, v)
+
+output = []
+for row in input["rows"]:
+    item = {}
+    for k, v in row.iteritems():
+        path = k.split('.')
+        path.reverse()
+        set_nested(item, path, v)
+    output.append(item)
