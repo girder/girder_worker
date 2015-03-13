398a1340df5bee33ad73f7de8f31ba18a2b8f316
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-03-13 13:26:27 -0400

    Adding ESGF facet querying

--- esgf/search.py
+++ esgf/search.py
@@ -1,10 +1,11 @@
 """ESGF Search API."""
 
+import six
 import requests
 
 
-def esgf_search(host, query):
-    """Search the given esgf host with the given query.
+def raw(host, query):
+    """Search the given esgf host with the given query object.
 
     :param str host: The host name (ex: ``"http://esg.ccs.ornl.gov/esg-search"``)
     :param dict query: The query parameters passed to the ESGF server
@@ -17,12 +18,14 @@ def esgf_search(host, query):
     `https://github.com/ESGF/esgf.github.io/wiki/ESGF_Search_REST_API`_
 
     Example:
-    >>> esgf_search('http://esg.ccs.ornl.gov/esg-search', {'project': 'ACME'})
+    >>> raw('http://esg.ccs.ornl.gov/esg-search', {'project': 'ACME'})
     ... # doctest: +ELLIPSIS
     {...}
-    >>> esgf_search('http://esg.ccs.ornl.gov/esg-search', {'query': 'climate'})
+    >>> raw('http://esg.ccs.ornl.gov/esg-search', {'badparam': ''})
     ... # doctest: +ELLIPSIS
-    {...}
+    Traceback (most recent call last):
+        ...
+    Exception: ESGF request failed with code ...
     """
 
     # Always return as a json object because nobody likes xml
@@ -34,3 +37,42 @@ def esgf_search(host, query):
         raise Exception("ESGF request failed with code {0}".format(req.status_code))
 
     return req.json()
+
+
+def facet(host, *fields):
+    """Search facets on the given ESGF server.
+
+    :param str host: The host name
+    :param str fields: The fields to search for, if none are given, return all fields
+    :return: A dictionary of fields -> ( values -> count )
+    :rtype: dict
+
+    Example:
+    >>> import json
+    >>> json.dumps(facet('http://esg.ccs.ornl.gov/esg-search', 'project'))
+    ... # doctest: +ELLIPSIS
+    '{"project": {...}}'
+    """
+    facets = '*'
+    if len(fields):
+        facets = ','.join(fields)
+
+    query = {
+        'limit': 0,  # don't return any file results
+        'facets': facets
+    }
+    resp = raw(host, query)
+
+    fields = resp['facet_counts']['facet_fields']
+
+    def pairs(l):
+        """Return pairs of the given list to construct a dict."""
+        i = 0
+        while i < len(l):
+            yield l[i], l[i + 1]
+            i += 2
+
+    for key, val in six.iteritems(fields):
+        fields[key] = dict(pairs(val))
+
+    return fields
