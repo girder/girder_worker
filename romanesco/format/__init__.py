b9f996133e670b0c71b350a21fbdda94a94899e1
Zack Galbreath <zack.galbreath@kitware.com>
2015-04-21 17:02:39 -0400

    fix failing pep8 test

--- romanesco/format/__init__.py
+++ romanesco/format/__init__.py
@@ -15,7 +15,8 @@ def csv_to_rows(input):
     # Special case: detect single-column files.
     # This check assumes that our only valid delimiters are commas and tabs.
     firstLine = input.split('\n')[0]
-    if not ('\t' in firstLine or ',' in firstLine) or len(input.splitlines()) == 1:
+    if not ('\t' in firstLine or ',' in firstLine) \
+            or len(input.splitlines()) == 1:
         dialect = 'excel'
 
     else:
