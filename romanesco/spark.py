4ed150964b87f28a92dedff100362bc424b2dd32
Chris Harris <chris.harris@kitware.com>
2015-06-10 14:58:59 -0400

    Guard against no "spark" config section

--- romanesco/spark.py
+++ romanesco/spark.py
@@ -3,29 +3,31 @@ import romanesco
 import os
 import sys
 
-from ConfigParser import ConfigParser, NoOptionError
+from ConfigParser import ConfigParser, NoOptionError, NoSectionError
 
 
 def setup_spark_env():
     # Setup pyspark
+    spark_home = None
+
     try:
         spark_home = romanesco.config.get('spark', 'spark_home')
+    except (NoOptionError, NoSectionError):
+        pass
 
-        # If not configured try the environment
-        if not spark_home:
-            spark_home = os.environ.get('SPARK_HOME')
+    # If not configured try the environment
+    if not spark_home:
+        spark_home = os.environ.get('SPARK_HOME')
 
-        if not spark_home:
-            raise Exception('spark_home must be set or SPARK_HOME must be set in '
-                            'the environment')
+    if not spark_home:
+        raise Exception('spark_home must be set or SPARK_HOME must be set in '
+                        'the environment')
 
-        # Need to set SPARK_HOME
-        os.environ['SPARK_HOME'] = spark_home
+    # Need to set SPARK_HOME
+    os.environ['SPARK_HOME'] = spark_home
 
-        if not os.path.exists(spark_home):
-            raise Exception('spark_home is not a valid directory')
-    except NoOptionError:
-        raise Exception('spark_home must be configured')
+    if not os.path.exists(spark_home):
+        raise Exception('spark_home is not a valid directory')
 
     sys.path.append(os.path.join(spark_home, 'python'))
     sys.path.append(os.path.join(spark_home, 'bin'))
@@ -38,8 +40,10 @@ def create_spark_context(task_spark_conf):
     from pyspark import SparkConf, SparkContext
     # Set can spark configuration parameter user has specified
     spark_conf = SparkConf()
-    for (name, value) in romanesco.config.items('spark'):
-        spark_conf.set(name, value)
+
+    if romanesco.config.has_section('spark'):
+        for (name, value) in romanesco.config.items('spark'):
+            spark_conf.set(name, value)
 
     # Override with any task specific configuration
     for (name, value) in task_spark_conf.items():
