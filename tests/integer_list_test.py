8e3acd67f30cff44c33ef4c9897998e3c56752c0
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-02-17 12:53:36 -0500

    Added new types - integer, integer_list, number_list, string_list - to support translation from xml sepc of slicer CLI

---
+++ tests/integer_list_test.py
@@ -0,0 +1,81 @@
+import girder_worker
+import unittest
+
+
+class TestIntegerList(unittest.TestCase):
+
+    def setUp(self):
+        self.analysis = {
+            "name": "concatenate",
+            "inputs": [
+                {
+                    "name": "a",
+                    "type": "integer_list",
+                    "format": "integer_list",
+                    "default": {
+                        "format": "json",
+                        "data": "[0]"
+                    }
+                },
+                {
+                    "name": "b",
+                    "type": "integer_list",
+                    "format": "integer_list"
+                }
+            ],
+            "outputs": [{"name": "c",
+                         "type": "integer_list", "format": "integer_list"}],
+            "script": "c = a + b",
+            "mode": "python"
+        }
+
+    def test_integer(self):
+        outputs = girder_worker.run(
+            self.analysis,
+            inputs={
+                "a": {"format": "integer_list", "data": [1, 2]},
+                "b": {"format": "integer_list", "data": [3, 4]}
+            },
+            outputs={
+                "c": {"format": "integer_list"}
+            })
+        self.assertEqual(outputs["c"]["format"], "integer_list")
+        self.assertEqual(outputs["c"]["data"], [1, 2, 3, 4])
+
+    def test_json(self):
+        outputs = girder_worker.run(
+            self.analysis,
+            inputs={
+                "a": {"format": "json", "data": "[1, 2]"},
+                "b": {"format": "json", "data": "[3, 4]"}
+            },
+            outputs={
+                "c": {"format": "json"}
+            })
+        self.assertEqual(outputs["c"]["format"], "json")
+        self.assertEqual(outputs["c"]["data"], "[1, 2, 3, 4]")
+
+    def test_default(self):
+        outputs = girder_worker.run(
+            self.analysis,
+            inputs={
+                "b": {"format": "integer_list", "data": [2]}
+            },
+            outputs={
+                "c": {"format": "integer_list"}
+            })
+        self.assertEqual(outputs["c"]["format"], "integer_list")
+        self.assertEqual(outputs["c"]["data"], [0, 2])
+
+        self.assertRaisesRegexp(
+            Exception, "^Required input 'b' not provided.$",
+            girder_worker.run, self.analysis,
+            inputs={
+                "a": {"format": "integer_list", "data": [2]}
+            },
+            outputs={
+                "c": {"format": "integer_list"}
+            })
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/string_list_test.py
@@ -0,0 +1,81 @@
+import girder_worker
+import unittest
+
+
+class TestStringList(unittest.TestCase):
+
+    def setUp(self):
+        self.analysis = {
+            "name": "concatenate",
+            "inputs": [
+                {
+                    "name": "a",
+                    "type": "string_list",
+                    "format": "string_list",
+                    "default": {
+                        "format": "json",
+                        "data": '["a"]'
+                    }
+                },
+                {
+                    "name": "b",
+                    "type": "string_list",
+                    "format": "string_list"
+                }
+            ],
+            "outputs": [{"name": "c",
+                         "type": "string_list", "format": "string_list"}],
+            "script": "c = a + b",
+            "mode": "python"
+        }
+
+    def test_string(self):
+        outputs = girder_worker.run(
+            self.analysis,
+            inputs={
+                "a": {"format": "string_list", "data": ["a", "b"]},
+                "b": {"format": "string_list", "data": ["c", "d"]}
+            },
+            outputs={
+                "c": {"format": "string_list"}
+            })
+        self.assertEqual(outputs["c"]["format"], "string_list")
+        self.assertEqual(outputs["c"]["data"], ["a", "b", "c", "d"])
+
+    def test_json(self):
+        outputs = girder_worker.run(
+            self.analysis,
+            inputs={
+                "a": {"format": "json", "data": '["a", "b"]'},
+                "b": {"format": "json", "data": '["c", "d"]'}
+            },
+            outputs={
+                "c": {"format": "json"}
+            })
+        self.assertEqual(outputs["c"]["format"], "json")
+        self.assertEqual(outputs["c"]["data"], '["a", "b", "c", "d"]')
+
+    def test_default(self):
+        outputs = girder_worker.run(
+            self.analysis,
+            inputs={
+                "b": {"format": "string_list", "data": ["b"]}
+            },
+            outputs={
+                "c": {"format": "string_list"}
+            })
+        self.assertEqual(outputs["c"]["format"], "string_list")
+        self.assertEqual(outputs["c"]["data"], ["a", "b"])
+
+        self.assertRaisesRegexp(
+            Exception, "^Required input 'b' not provided.$",
+            girder_worker.run, self.analysis,
+            inputs={
+                "a": {"format": "string_list", "data": ["a"]}
+            },
+            outputs={
+                "c": {"format": "string_list"}
+            })
+
+if __name__ == '__main__':
+    unittest.main()
