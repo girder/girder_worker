a7ff4e5345a380c8a095fb1265ccaaaade746f93
Zach Mullen <zach.mullen@kitware.com>
2016-04-22 11:48:55 -0400

    Must sudo creation of system groups and users

--- girder_worker/plugins/docker/scripts/entrypoint.sh
+++ girder_worker/plugins/docker/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Usage: entrypoint.sh <user_id> <group_id> <command to run>
-groupadd -o -g $2 -r worker
-useradd -o -u $1 --create-home -r -g worker worker
+sudo groupadd -o -g $2 -r worker
+sudo useradd -o -u $1 --create-home -r -g worker worker
 exec ${@:3}
