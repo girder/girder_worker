88cd72ec59af32274087cffe7c39b53d882dff75
Jeffrey Baumes <jeff.baumes@kitware.com>
2016-04-26 14:02:32 -0400

    Add method for converting nested to newick using ete3

---
+++ girder_worker/format/tree/nested_to_newick.py
@@ -0,0 +1,13 @@
+from ete3 import Tree
+
+
+def populate_tree(node, children):
+    for c in children:
+        name = c.get('node_data', {}).get('node name', '')
+        dist = c.get('edge_data', {}).get('weight')
+        n = node.add_child(name=name, dist=dist)
+        populate_tree(n, c.get('children', []))
+
+t = Tree()
+populate_tree(t, input.get('children', []))
+output = t.write(format=1)
