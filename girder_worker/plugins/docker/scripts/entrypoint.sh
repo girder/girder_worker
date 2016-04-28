f7792578123a2c3dacd7d3b1bdd99d568cd879cc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-04-28 12:10:19 -0400

    docker/entrypoint.sh: Must add quotes around arguments with spaces
    This commit allows to run a docker container specifying command
    like these one:
    
      docker run -ti imagename --entrypoint /path/to/entrypoint.sh bach -c "echo 'Hello'"
    
    Co-authored-by: Deepak Chittajallu <deepak.chittajallu@kitware.com>
    Co-authored-by: Zach Mullen <zach.mullen@kitware.com>

--- girder_worker/plugins/docker/scripts/entrypoint.sh
+++ girder_worker/plugins/docker/scripts/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 # Usage: entrypoint.sh <user_id> <group_id> <command to run>
-groupadd -o -g $2 -r worker
-useradd -o -u $1 --create-home -r -g worker worker
-exec sudo -u worker ${@:3}
+
+# Add 'worker' user and group matching requested user_id and group_id
+group_id=$2
+user_id=$1
+groupadd -o -g $group_id -r worker
+useradd -o -u $user_id --create-home -r -g worker worker
+
+# Requote arguments to account for argument with spaces
+quoted_args=''
+for i in "${@:3}"; do
+    quoted_args="$quoted_args \"${i//\"/\\\"}\""
+done
+
+# Execute command as `worker` user
+export HOME=/home/worker
+exec su -m worker -c "$quoted_args"
