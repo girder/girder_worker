e6821be2c834e4f3fa9327e6647e67602c1251b1
Christopher Kotfila <chris.kotfila@kitware.com>
2016-10-31 14:57:24 -0400

    Fix spark test (maybe:redux)

--- girder_worker/plugins/spark/__init__.py
+++ girder_worker/plugins/spark/__init__.py
@@ -1,6 +1,4 @@
 import os
-import girder_worker
-from . import pyspark_executor, spark
 
 SC_KEY = '_girder_worker_spark_context'
 
@@ -10,6 +8,7 @@ def setup_pyspark_task(event):
     This is executed before a task execution. If it is a pyspark task, we
     create the spark context here so it can be used for any input conversion.
     """
+    from . import spark
     info = event.info
     if info['mode'] == 'spark.python' and SC_KEY not in info['kwargs']:
         spark_conf = info['task'].get('spark_conf', {})
@@ -23,14 +22,18 @@ def pyspark_run_cleanup(event):
 
 
 def load(params):
+    import girder_worker
+    from girder_worker.core import events, format, register_executor
+    from . import pyspark_executor, spark
+
     # If we have a spark config section then try to setup spark environment
     if girder_worker.config.has_section('spark') or 'SPARK_HOME' in os.environ:
         spark.setup_spark_env()
 
-    girder_worker.register_executor('spark.python', pyspark_executor.run)
+    register_executor('spark.python', pyspark_executor.run)
 
-    girder_worker.events.bind('run.before', 'spark', setup_pyspark_task)
-    girder_worker.events.bind('run.finally', 'spark', pyspark_run_cleanup)
+    events.bind('run.before', 'spark', setup_pyspark_task)
+    events.bind('run.finally', 'spark', pyspark_run_cleanup)
 
-    girder_worker.format.import_converters(
+    format.import_converters(
         os.path.join(params['plugin_dir'], 'converters'))
