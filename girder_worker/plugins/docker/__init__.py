352ded17f5dae7cbb0628506bc74796e6a3bf6d5
Zach Mullen <zach.mullen@kitware.com>
2017-01-10 15:28:28 -0500

    Closes #74. Do not issue platform exception for docker
    There are now native docker executables for other platforms besides Linux.

--- girder_worker/plugins/docker/__init__.py
+++ girder_worker/plugins/docker/__init__.py
@@ -1,7 +1,3 @@
-import os
-import platform
-
-
 def before_run(e):
     import executor
     if e.info['task']['mode'] == 'docker':
@@ -12,10 +8,5 @@ def load(params):
     from girder_worker.core import events, register_executor
     import executor
 
-    if (platform.system() != 'Linux' and
-            not os.environ.get('WORKER_FORCE_DOCKER_START')):
-        raise Exception('The docker plugin only works on Linux hosts due to '
-                        'mapping of shared volumes and pipes between host and '
-                        'container.')
     events.bind('run.before', 'docker', before_run)
     register_executor('docker', executor.run)
