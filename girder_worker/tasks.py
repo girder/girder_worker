13bd7be5e88e897623799d200d9466d01ba62688
Zach Mullen <zach.mullen@kitware.com>
2017-01-14 16:50:56 -0500

    Move format/type logic into a new plugin "types"

--- girder_worker/tasks.py
+++ girder_worker/tasks.py
@@ -6,7 +6,6 @@ from .app import app
 @app.task(name='girder_worker.run')
 def run(*pargs, **kwargs):
     jobInfo = kwargs.pop('jobInfo', {})
-    retval = 0
 
     with JobManager(logPrint=jobInfo.get('logPrint', True),
                     url=jobInfo.get('url'), method=jobInfo.get('method'),
@@ -14,25 +13,4 @@ def run(*pargs, **kwargs):
                     reference=jobInfo.get('reference')) as jm:
         kwargs['_job_manager'] = jm
         kwargs['status'] = JobStatus.RUNNING
-        retval = core.run(*pargs, **kwargs)
-        return retval
-
-
-@app.task(name='girder_worker.convert')
-def convert(*pargs, **kwargs):
-    return core.convert(*pargs, **kwargs)
-
-
-@app.task(name='girder_worker.validators')
-def validators(*pargs, **kwargs):
-    _type, _format = pargs
-    nodes = []
-
-    for (node, data) in core.format.conv_graph.nodes(data=True):
-        if ((_type is None) or (_type == node.type)) and \
-           ((_format is None) or (_format == node.format)):
-            nodes.append({'type': node.type,
-                          'format': node.format,
-                          'validator': data})
-
-    return nodes
+        return core.run(*pargs, **kwargs)
