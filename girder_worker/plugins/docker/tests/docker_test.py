fa28e2b78a5b33bdc7be94b47653946ab0d5220e
Chris Harris <chris.harris@kitware.com>
2017-01-31 11:44:34 -0500

    Update docker_test to expect docker-py calls

--- girder_worker/plugins/docker/tests/docker_test.py
+++ girder_worker/plugins/docker/tests/docker_test.py
@@ -17,8 +17,20 @@ OUT_FD, ERR_FD = 100, 200
 _out = six.StringIO('output message\n')
 _err = six.StringIO('error message\n')
 
-processMock = mock.Mock()
-processMock.configure_mock(**{
+stdout_socket_mock = mock.Mock()
+stdout_socket_mock.fileno.return_value = OUT_FD
+
+stderr_socket_mock = mock.Mock()
+stderr_socket_mock.fileno.return_value = ERR_FD
+
+docker_container_mock = mock.Mock()
+docker_container_mock.attach_socket.side_effect = [stdout_socket_mock, stderr_socket_mock]
+
+docker_client_mock = mock.Mock()
+docker_client_mock.containers.run.return_value = docker_container_mock
+
+process_mock = mock.Mock()
+process_mock.configure_mock(**{
     'communicate.return_value': ('', ''),
     'poll.return_value': 0,
     'stdout.fileno.return_value': OUT_FD,
@@ -26,6 +38,11 @@ processMock.configure_mock(**{
     'returncode': 0
 })
 
+def _reset_mocks():
+    global docker_client_mock, docker_container_mock
+    docker_client_mock.reset_mock()
+    # Need to reset side_effect
+    docker_container_mock.attach_socket.side_effect = [stdout_socket_mock, stderr_socket_mock]
 
 # Monkey patch select.select in the docker task module
 def _mockSelect(r, w, x, *args, **kwargs):
@@ -63,9 +80,14 @@ def tearDownModule():
 
 
 class TestDockerMode(unittest.TestCase):
+    def setUp(self):
+        _reset_mocks()
+
     @mock.patch('subprocess.Popen')
-    def testDockerMode(self, mockPopen):
-        mockPopen.return_value = processMock
+    @mock.patch('docker.from_env')
+    def testDockerMode(self, from_env, popen):
+        popen.return_value = process_mock
+        from_env.return_value = docker_client_mock
 
         task = {
             'mode': 'docker',
@@ -136,29 +158,41 @@ class TestDockerMode(unittest.TestCase):
                 }
             })
 
-            self.assertEqual(mockPopen.call_count, 3)
-            cmd1, cmd2, cmd3 = [x[1]['args'] for x in mockPopen.call_args_list]
+            # We should have one call to images.pull(...)
+            self.assertEqual(docker_client_mock.images.pull.call_count, 1)
+            self.assertEqual(docker_client_mock.images.pull.call_args_list[0][0], ('test/test:latest', ))
 
-            self.assertEqual(cmd1, ('docker', 'pull', 'test/test:latest'))
-            self.assertEqual(cmd2[:3], ['docker', 'run', '-v'])
-            six.assertRegex(self, cmd2[3], _tmp + '/.*:%s' % DATA_VOLUME)
-            self.assertEqual(cmd2[4:7], [
-                'test/test:latest', '-f', '%s/file.txt' % DATA_VOLUME])
-            self.assertEqual(cmd2[-2], '--temp-dir=%s' % DATA_VOLUME)
-            self.assertEqual(cmd2[-1], '--bar')
-            self.assertEqual(len(cmd2), 9)
 
-            self.assertEqual(cmd3[:4], ['docker', 'run', '--rm', '-v'])
-            six.assertRegex(self, cmd3[4], _tmp + '/.*:%s' % DATA_VOLUME)
-            self.assertEqual(cmd3[5:], ['busybox', 'chmod', '-R', 'a+rw', DATA_VOLUME])
-            self.assertEqual(len(cmd3), 10)
+            # We should have two calls to containers.run(...)
+            self.assertEqual(docker_client_mock.containers.run.call_count, 2)
+            run1, run2 = docker_client_mock.containers.run.call_args_list
+
+
+            args, kwargs = run1
+            self.assertEqual(args[0], 'test/test:latest')
+            six.assertRegex(self, kwargs['volumes'].keys()[0], _tmp + '/.*')
+            self.assertEqual(kwargs['volumes'].itervalues().next()['bind'], DATA_VOLUME)
+            self.assertEqual(args[1][0:2], ['-f', '%s/file.txt' % DATA_VOLUME])
+            self.assertEqual(args[1][-2], '--temp-dir=%s' % DATA_VOLUME)
+            self.assertEqual(args[1][-1], '--bar')
+
+            # Call to docker-gc
+            popen_calls = popen.call_args_list
+            self.assertEqual(len(popen_calls), 1)
+            six.assertRegex(self, popen_calls[0][1]['args'][0], 'docker-gc$')
+
+            args, kwargs = run2
+            self.assertEqual(args[0], 'busybox')
+
+            self.assertTrue(kwargs['remove'])
+            six.assertRegex(self, kwargs['volumes'].keys()[0], _tmp + '/.*')
+            self.assertEqual(kwargs['volumes'].itervalues().next()['bind'], DATA_VOLUME)
+            self.assertEqual(args[1], ['chmod', '-R', 'a+rw', DATA_VOLUME])
 
             # Make sure we can specify a custom entrypoint to the container
-            mockPopen.reset_mock()
-            task['entrypoint'] = '/bin/bash'
+            _reset_mocks()
 
-            # Make sure additional docker run args work
-            task['docker_run_args'] = ['--net', 'none']
+            task['entrypoint'] = '/bin/bash'
 
             inputs['foo'] = {
                 'mode': 'http',
@@ -169,14 +203,22 @@ class TestDockerMode(unittest.TestCase):
                 'data': False
             }
             run(task, inputs=inputs, validate=False, auto_convert=False)
-            self.assertEqual(mockPopen.call_count, 3)
-            cmd2 = mockPopen.call_args_list[1][1]['args']
-            self.assertEqual(cmd2[4:9], [
-                '--net', 'none', '--entrypoint', '/bin/bash', 'test/test:latest'])
-            self.assertNotIn('--bar', cmd2)
-            self.assertEqual(cmd2[9:11], ['-f', '%s/file.txt' % DATA_VOLUME])
 
-            mockPopen.reset_mock()
+            self.assertEqual(docker_client_mock.containers.run.call_count, 2)
+            args, kwargs = docker_client_mock.containers.run.call_args_list[0]
+            self.assertEqual(args[0], 'test/test:latest')
+            self.assertEqual(kwargs['entrypoint'], ['/bin/bash'])
+
+            self.assertNotIn('--bar', args)
+            self.assertEqual(args[1][0:2], ['-f', '%s/file.txt' % DATA_VOLUME])
+
+            _reset_mocks()
+            popen.reset_mock()
+
+            # Make sure custom config settings are respected
+            girder_worker.config.set('docker', 'cache_timeout', '123456')
+            girder_worker.config.set(
+                'docker', 'exclude_images', 'test/test:latest')
 
             # Make sure we can skip pulling the image
             task['pull_image'] = False
@@ -185,15 +227,23 @@ class TestDockerMode(unittest.TestCase):
                 'url': 'https://foo.com/file.txt'
             }
             run(task, inputs=inputs, validate=False, auto_convert=False)
-            self.assertEqual(mockPopen.call_count, 2)
-            cmd1 = [x[1]['args'] for x in mockPopen.call_args_list][0]
-            self.assertEqual(tuple(cmd1[:2]), ('docker', 'run'))
-            self.assertEqual(cmd1[4:6], ['--net', 'none'])
+
+            # Assert no call to images.pull
+            self.assertEqual(docker_client_mock.images.pull.call_count, 0)
+            self.assertEqual(docker_client_mock.containers.run.call_count, 2)
+
+            popen_calls = popen.call_args_list
+            self.assertEqual(len(popen_calls), 1)
+            six.assertRegex(self, popen_calls[0][1]['args'][0], 'docker-gc$')
+
+            env = popen_calls[0][1]['env']
+            self.assertEqual(env['GRACE_PERIOD_SECONDS'], '123456')
+            six.assertRegex(self, env['EXCLUDE_FROM_GC'], 'docker_gc_scratch/.docker-gc-exclude$')
 
     @mock.patch('subprocess.Popen')
     def testCleanupHook(self, mockPopen):
         os.makedirs(_tmp)
-        mockPopen.return_value = processMock
+        mockPopen.return_value = process_mock
         girder_worker.config.set('docker', 'cache_timeout', '123456')
         girder_worker.config.set('docker', 'exclude_images', 'test/test:latest')
 
@@ -209,8 +259,10 @@ class TestDockerMode(unittest.TestCase):
         six.assertRegex(self, env['EXCLUDE_FROM_GC'], r'\.docker-gc-exclude$')
 
     @mock.patch('subprocess.Popen')
-    def testOutputValidation(self, mockPopen):
-        mockPopen.return_value = processMock
+    @mock.patch('docker.from_env')
+    def testOutputValidation(self, from_env, popen):
+        popen.return_value = process_mock
+        from_env.return_value = docker_client_mock
 
         task = {
             'mode': 'docker',
@@ -229,6 +281,7 @@ class TestDockerMode(unittest.TestCase):
         with self.assertRaisesRegexp(TaskSpecValidationError, msg):
             run(task)
 
+        _reset_mocks()
         task['outputs'][0]['target'] = 'filepath'
         task['outputs'][0]['path'] = '/tmp/some/invalid/path'
         msg = (r'^Docker filepath output paths must either start with "%s/" '
@@ -242,7 +295,8 @@ class TestDockerMode(unittest.TestCase):
         with self.assertRaisesRegexp(Exception, msg):
             run(task)
         # Make sure docker stuff actually got called in this case.
-        self.assertEqual(mockPopen.call_count, 3)
+
+        self.assertEqual(docker_client_mock.containers.run.call_count, 2)
 
         # Simulate a task that has written into the temp dir
         tmp = os.path.join(_tmp, 'simulated_output')
@@ -251,6 +305,7 @@ class TestDockerMode(unittest.TestCase):
         path = os.path.join(tmp, 'valid_path.txt')
         with open(path, 'w') as f:
             f.write('simulated output')
+        _reset_mocks()
         outputs = run(task, _tempdir=tmp)
         self.assertEqual(outputs, {
             'file_output_1': {
@@ -258,7 +313,7 @@ class TestDockerMode(unittest.TestCase):
                 'format': 'text'
             }
         })
-
+        _reset_mocks()
         # If no path is specified, we should fall back to the input name
         del task['outputs'][0]['path']
         path = os.path.join(_tmp, '.*', 'file_output_1')
@@ -266,11 +321,11 @@ class TestDockerMode(unittest.TestCase):
         with self.assertRaisesRegexp(Exception, msg):
             run(task)
 
-    @mock.patch('girder_worker.core.utils.run_process')
     @mock.patch('subprocess.Popen')
-    def testNamedPipes(self, mockPopen, mockRunProcess):
-        mockRunProcess.return_value = processMock
-        mockPopen.return_value = processMock
+    @mock.patch('docker.from_env')
+    def testNamedPipes(self, from_env, popen):
+        popen.return_value = process_mock
+        from_env.return_value = docker_client_mock
 
         task = {
             'mode': 'docker',
