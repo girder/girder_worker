330666c246f623beb0c55e6282917dc507b6fbc7
Zach Mullen <zach.mullen@kitware.com>
2017-04-11 14:19:02 -0400

    v0.3.1

--- girder_worker/__init__.py
+++ girder_worker/__init__.py
@@ -3,7 +3,7 @@ import os
 from . import log_utils
 from ConfigParser import SafeConfigParser
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __license__ = 'Apache 2.0'
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
