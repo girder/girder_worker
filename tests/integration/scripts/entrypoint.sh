a8946ea8a94e190b827ec792dd74a259ee2f5330
Christopher Kotfila <chris.kotfila@kitware.com>
2017-05-28 10:52:02 -0400

    Add GW container and common_tasks GW task plugin to the mix.
    Note that common_tasks.egg-info must exist and be version
    controlled. Both girder & girder_worker containers create egg-link
    files in /usr/local/lib/python2.7/dist-packages that point to
    /girder_worker/tests/integration/common_tasks. This means if you mount
    common_tasks at run time there must be an egg-info folder for the
    package to be importable (and for entry points to be found).

--- tests/integration/scripts/entrypoint.sh
+++ tests/integration/scripts/entrypoint.sh
@@ -13,6 +13,14 @@ pushd /scripts
 ansible-playbook -i inventory setup.yml
 popd
 
+# If /girder_worker/setup.py exists then we've
+# mounted girder_worker at run time,  make sure it
+# is properly installed before continuing
+if [ -e /girder_worker/setup.py ]; then
+    pip uninstall -y girder-worker
+    pip install -e /girder_worker
+fi
+
 # Tear down Girder
 kill $(pgrep -f girder)
 
