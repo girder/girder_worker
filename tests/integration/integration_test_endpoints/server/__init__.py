971d897c826b08c6da6cc50cff6459a42d0bf54d
Christopher Kotfila <chris.kotfila@kitware.com>
2017-05-30 11:07:56 -0400

    Add first example end to end test endpoint

--- tests/integration/integration_test_endpoints/server/__init__.py
+++ tests/integration/integration_test_endpoints/server/__init__.py
@@ -1,21 +1,27 @@
 from girder.api import access
 from girder.api.describe import Description, describeRoute
 from girder.api.rest import Resource
-
+from common_tasks.test_tasks.fib import fibonacci
 
 class IntegrationTestEndpoints(Resource):
     def __init__(self):
         super(IntegrationTestEndpoints, self).__init__()
         self.resourceName = 'integration_tests'
 
-        self.route('POST', ('test', ), self.test)
+        # Automatically add routes for any endpoint
+        # that begins with 'test_'
+        for attr in dir(self):
+            if attr.startswith('test_'):
+                endpoint = getattr(self, attr)
+                if hasattr(endpoint, '__call__'):
+                    self.route('POST', (attr, ), endpoint)
 
     @access.token
     @describeRoute(
-        Description('Get an asyn result')
-    )
-    def test(self, params):
-        return 'SUCCESS'
+        Description('Test celery task delay'))
+    def test_celery_task_delay(self, params):
+        result = fibonacci.delay(20)
+        return result.job
 
 
 def load(info):
