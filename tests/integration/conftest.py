25cff79856b87f114e9b991acd143bc172ca45b2
Christopher Kotfila <chris.kotfila@kitware.com>
2017-05-30 14:44:58 -0400

    Add initial end to end test of delay() function

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,11 @@
+import warnings
 import pytest
 import requests
+import contextlib
+import time
+import functools
+from girder_worker.utils import JobStatus
+from datetime import timedelta, datetime
 
 
 @pytest.fixture(scope='module')
@@ -29,6 +35,65 @@ def session(api_url, request):
                 'Unable to login with user "{}", password "{}"'.format(*login))
 
         yield s
+
+
+@pytest.fixture
+def wait_for(session, api_url):
+
+    @contextlib.contextmanager
+    def _wait_for(job_id, predicate, timeout=20, interval=0.3, on_timeout=None):
+        then = datetime.utcnow() + timedelta(seconds=timeout)
+        timeout = True
+
+        while datetime.utcnow() < then:
+            r = session.get(api_url('job/{}'.format(job_id)))
+
+            if predicate(r.json()):
+                timeout = False
+                break
+
+            time.sleep(interval)
+
+        r = session.get(api_url('job/{}'.format(job_id)))
+
+        if timeout:
+            if on_timeout is None:
+                def on_timeout(j):
+                    return 'Timed out waiting for {}'.format(
+                        api_url('job/{}'.format(j['_id'])))
+
+            warnings.warn(on_timeout(r.json()))
+
+        yield r.json()
+
+    return _wait_for
+
+
+@pytest.fixture
+def wait_for_success(wait_for, api_url):
+
+    def on_timeout(j):
+        return 'Timed out waiting for {} to move into success state'.format(
+            api_url('job/{}'.format(j['_id'])))
+
+    return functools.partial(
+        wait_for,
+        predicate=lambda j: j['status'] == JobStatus.SUCCESS,
+        on_timeout=on_timeout)
+
+
+@pytest.fixture
+def wait_for_error(wait_for, api_url):
+
+    def on_timeout(j):
+        return 'Timed out waiting for {} to move into error state'.format(
+            api_url('job/{}'.format(j['_id'])))
+
+    return functools.partial(
+        wait_for,
+        predicate=lambda j: j['status'] == JobStatus.ERROR,
+        on_timeout=on_timeout)
+
 
 
 # pytest hook for ordering test items after they have been
--- tests/integration/test_celery.py
+++ tests/integration/test_celery.py
@@ -1,3 +1,4 @@
+from girder_worker.utils import JobStatus
 import pytest
 
 @pytest.mark.priority(10)
@@ -7,3 +8,12 @@ def test_session(session, api_url):
     assert r.json()['login'] == 'admin'
     assert r.json()['admin'] == True
     assert r.json()['status'] == 'enabled'
+
+
+def test_celery_task_delay(session, api_url, wait_for_success):
+    r = session.post(api_url('integration_tests/test_celery_task_delay'))
+    assert r.status_code == 200
+
+    with wait_for_success(r.json()['_id']) as job:
+        assert [ts['status'] for ts in job['timestamps']] == \
+            [JobStatus.RUNNING, JobStatus.SUCCESS]
