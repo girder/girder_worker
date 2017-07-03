66843a190ca50f1b8de0805e1a292cb6d738eebd
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-07-03 08:31:53 -0400

    Add circleci support to run the integration tests

--- tests/integration/common_tasks/common_tasks/__init__.py
+++ tests/integration/common_tasks/common_tasks/__init__.py
@@ -5,21 +5,6 @@ class CommonTasksPlugin(GirderWorkerPluginABC):
     def __init__(self, app, *args, **kwargs):
         self.app = app
 
-        # Note: within the context of the executing docker test
-        # environment the RabbitMQ server is addressable as 'rabbit.'
-        # Usually we statically configure the broker url in
-        # worker.local.cfg or fall back to worker.dist.cfg.  In this
-        # case however we are mounting the local girder_worker
-        # checkout inside the docker containers and don't want to
-        # surprise users by programatically modifying their
-        # configuration from the docker container's entrypoint. To
-        # solve this we set the broker URL for the girder_worker app
-        # inside the girder_worker container here.
-
-        self.app.conf.update({
-            'broker_url': 'amqp://guest:guest@rabbit/'
-        })
-
     def task_imports(self):
         # Return a list of python importable paths to the
         # plugin's path directory
