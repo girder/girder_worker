4becf664234f113283f1ce0d1118f2acbc897772
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-07-12 11:37:55 -0400

    Replace custom entrypoint handling with stevedore

--- girder_worker/entrypoint.py
+++ girder_worker/entrypoint.py
@@ -1,35 +1,11 @@
 from importlib import import_module
-import pkg_resources as pr
 
-from .app import app
+from stevedore import extension
 
+from app import app
 
-def get_plugin_tasks(ep):
-    """Return a list of modules containing plugin tasks.
-
-    :param ep: An entrypoint object defined by a plugin
-    :returns: A list of importable modules
-    """
-    try:
-        plugin_class = ep.load()
-
-    except Exception:
-        import traceback
-        traceback.print_exc()
-        print('Could not load \'%s\', skipping.' % str(ep))
-        return []
-
-    try:
-        plugin = plugin_class(app)
-        return plugin.task_imports()
-
-    except Exception:
-        import traceback
-        traceback.print_exc()
-        print(
-            'Problem instantiating plugin %s, skipping.'
-            % plugin_class.__name__)
-        return []
+#: Defines the namespace used for plugin entrypoints
+NAMESPACE = 'girder_worker_plugins'
 
 
 def _import_module(module):
@@ -42,36 +18,43 @@ def _import_module(module):
         print('Problem importing %s' % module)
 
 
-def _get_entrypoints():
-    """Get a list of entrypoints from package resources.
+def _handle_entrypoint_errors(mgr, entrypoint, exc):
+    print('Problem loading plugin %s, skipping' % entrypoint.name)
 
-    :returns:
-        A tuple whose first element is the core entrypoint
-        and whose second element is a list of entrypoints
-        provided by plugins.
-    """
-    entrypoints = []
-    core = None
-    for ep in pr.iter_entry_points(group='girder_worker_plugins'):
-        # If this is the girder_worker EntryPoint
-        if ep.module_name == __package__:
-            core = ep
-        else:
-            entrypoints.append(ep)
-    return core, entrypoints
+
+def get_extension_manager():
+    """Get an extension manager for the plugin namespace."""
+    return extension.ExtensionManager(
+        namespace=NAMESPACE,
+        invoke_on_load=True,
+        invoke_args=(app,),
+        on_load_failure_callback=_handle_entrypoint_errors
+    )
+
+
+def get_task_imports(ext):
+    """Return a list of task modules provided by an extension."""
+    try:
+        includes = ext.obj.task_imports()
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        print('Problem instantiating plugin %s, skipping' % ext.name)
+        includes = []
+    return includes
 
 
 def get_core_task_modules():
     """Return task modules defined by core."""
-    core = _get_entrypoints()[0]
-    return get_plugin_tasks(core)
+    return get_task_imports(get_extension_manager()['core'])
 
 
 def get_plugin_task_modules():
     """Return task modules defined by plugins."""
     includes = []
-    for entrypoint in _get_entrypoints()[1]:
-        includes.extend(get_plugin_tasks(entrypoint))
+    for ext in get_extension_manager():
+        if ext.name != 'core':
+            includes.extend(get_task_imports(ext))
     return includes
 
 
