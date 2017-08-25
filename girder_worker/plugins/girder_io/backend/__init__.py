fd0e857ae52f2cd78af256c2aaf557f68b7e8bdd
Chris Harris <chris.harris@kitware.com>
2017-08-25 13:23:14 -0400

    Refactor to take in account running within Girder

--- girder_worker/plugins/girder_io/backend/__init__.py
+++ girder_worker/plugins/girder_io/backend/__init__.py
@@ -21,10 +21,12 @@ class GirderBackend(BaseBackend):
                  parent_type=None, **kwargs):
         super(GirderBackend, self).__init__(app, url=url,  **kwargs)
 
-
-        if self.url is None:
-            raise ImproperlyConfigured(
-                'url for Girder server must be provided.')
+        self._girder = None
+        if not self._in_girder:
+            # If we are outside Girder we need a url
+            if self.url is None:
+                raise ImproperlyConfigured(
+                        'url for Girder server must be provided.')
 
         self._token = token
         self._api_key = api_key
@@ -34,7 +36,7 @@ class GirderBackend(BaseBackend):
 
         parts = urlparse(url)
         query_params = dict(parse_qsl(parts.query))
-        print(query_params)
+
         # Give precedence to parameter passed in
         if self._token is None:
             self._token = query_params.get('token')
@@ -59,6 +61,18 @@ class GirderBackend(BaseBackend):
                 'A parent resource must be provided.')
 
     @property
+    def _in_girder(self):
+        if self._girder is None:
+            self._girder = False
+            try:
+                from girder.utility.model_importer import ModelImporter
+                self._girder = True
+            except ImportError:
+                pass
+
+        return self._girder
+
+    @property
     def _client(self):
         if self._gclient is None:
             self._gclient = GirderClient(apiUrl=self.url)
@@ -71,6 +85,13 @@ class GirderBackend(BaseBackend):
 
     def _store_result(self, task_id, result, state,
                       traceback=None, request=None, **kwargs):
+
+        client = self._client
+
+        # If the request has an url and token use these
+        if hasattr(request, 'girder_api_url') and hasattr(request, 'girder_client_token'):
+            client = GirderClient(apiUrl=request.girder_api_url)
+            client.token = request.girder_client_token
 
         result_meta = {
             'status': state,
@@ -89,58 +110,161 @@ class GirderBackend(BaseBackend):
 
         return result
 
-    def _forget(self, task_id):
+    def _forget_girder(self, task_id):
+        from girder.utility.model_importer import ModelImporter
+        from girder.api.rest import getCurrentUser
+        from girder.constants import AccessType
+        folder = ModelImporter.model('folder').load(
+                id=self._parent_id, user=getCurrentUser(), level=AccessType.ADMIN)
+
+        filters = {
+            'name': task_id
+        }
+        items = list(ModelImporter.model('folder').childItems(
+                     folder=folder, limit=1, filters=filters))
+
+        if len(items) == 1:
+            item = items[0]
+            ModelImporter.model('item').remove(item)
+
+    def _forget_girder_client(self, task_id):
         items = list(self._client.listItem(self._parent_id, name=task_id, limit=1))
         if len(items) == 1:
             item = items[0]
             self._client.delete('item/%s' % item['_id'])
 
+    def _forget(self, task_id):
+        if self._in_girder:
+            self._forget_girder(task_id)
+        else:
+            self._forget_girder_client(task_id)
 
-    def _save_group(self, group_id, result):
-        print('save group')
-        group_meta = {
+    def _group_meta(self, result):
+        return {
             'result': self.encode([i.id for i in result]),
             'timestamp': datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat()
         }
 
-        if self._parent_type == 'folder':
-            # Change to one call then PR is merged into master
-            item = self._client.createItem(self._parent_id, group_id, reuseExisting=False)
-            self._client.addMetadataToItem(item['_id'], group_meta)
+    def _save_group_girder(self, group_id, result):
+        from girder.utility.model_importer import ModelImporter
+        from girder.api.rest import getCurrentUser
+        from girder.constants import AccessType
+        folder = ModelImporter.model('folder').load(
+                id=self._parent_id, user=getCurrentUser(), level=AccessType.WRITE)
+
+        item_model = ModelImporter.model('item')
+        item = item_model.createItem(group_id, getCurrentUser(),
+                                     folder)
+        item_model.setMetadata(item, self._group_meta(result))
 
         return result
+
+    def _save_group_girder_client(self, group_id, result):
+        group_meta = self._group_meta(result)
+        # Change to one call then PR is merged into master
+        item = self._client.createItem(self._parent_id, group_id, reuseExisting=False)
+        self._client.addMetadataToItem(item['_id'], group_meta)
+
+        return result
+
+    def _save_group(self, group_id, result):
+        if self._in_girder:
+            return self._save_group_girder(group_id, result)
+        else:
+            return self._save_group_girder_client(group_id, result)
 
     def _delete_group(self, group_id):
         self._forget(group_id)
 
-    def _get_task_meta_for(self, task_id):
+    def _task_meta_from_item(self, item):
+        print(item)
+        meta = item['meta']
+
+        return self.meta_from_decoded({
+            'task_id': item['name'],
+            'status': meta['status'],
+            'result': self.decode(meta['result']),
+            'timestamp': meta['timestamp'],
+            'traceback': self.decode(meta['traceback']),
+            'children': self.decode(meta['children']),
+        })
+
+    def _get_task_meta_for_girder(self, task_id):
+        from girder.utility.model_importer import ModelImporter
+        from girder.api.rest import getCurrentUser
+        from girder.constants import AccessType
+        folder = ModelImporter.model('folder').load(
+                id=self._parent_id, user=getCurrentUser(), level=AccessType.READ)
+
+        filters = {
+            'name': task_id
+        }
+        items = list(ModelImporter.model('folder').childItems(
+                     folder=folder, limit=1, filters=filters))
+
+        if len(items) == 1:
+            item = items[0]
+            return self._task_meta_from_item(item)
+
+        return {'status': states.PENDING, 'result': None}
+
+    def _get_task_meta_for_girder_client(self, task_id):
         items = list(self._client.listItem(self._parent_id, name=task_id, limit=1))
         if len(items) == 1:
             item = items[0]
-            meta = item['meta']
 
-            return self.meta_from_decoded({
-                'task_id': item['name'],
-                'status': meta['status'],
-                'result': self.decode(meta['result']),
-                'timestamp': meta['timestamp'],
-                'traceback': self.decode(meta['traceback']),
-                'children': self.decode(meta['children']),
-            })
+            return self._task_meta_from_item(item)
+
+        return {'status': states.PENDING, 'result': None}
+
+    def _get_task_meta_for(self, task_id):
+        if self._in_girder:
+            return self._get_task_meta_for_girder(task_id)
+        else:
+            return self._get_task_meta_for_girder_client(task_id)
+
+    def _group_meta_from_item(self, item):
+        meta = item['meta']
+
+        return {
+            'task_id': item['name'],
+            'timestamp': meta['timestamp'],
+            'result': [
+                self.app.AsyncResult(task)
+                for task in self.decode(meta['result'])
+                ],
+        }
+
+    def _restore_group_girder(self, group_id):
+        from girder.utility.model_importer import ModelImporter
+        from girder.api.rest import getCurrentUser
+        from girder.constants import AccessType
+        folder = ModelImporter.model('folder').load(
+                id=self._parent_id, user=getCurrentUser(), level=AccessType.READ)
+
+        filters = {
+            'name': group_id
+        }
+        items = list(ModelImporter.model('folder').childItems(
+                     folder=folder, limit=1, filters=filters))
+
+        if len(items) == 1:
+            item = items[0]
+            return self._group_meta_from_item(item)
+
+        return {'status': states.PENDING, 'result': None}
+
+    def _restore_group_grider_client(self, group_id):
+        items = list(self._client.listItem(self._parent_id, name=group_id, limit=1))
+        if len(items) == 1:
+            item = items[0]
+
+            return self._group_meta_from_item(item)
 
         return {'status': states.PENDING, 'result': None}
 
     def _restore_group(self, group_id):
-        items = list(self._client.listItem(self._parent_id, name=group_id, limit=1))
-        if len(items) == 1:
-            item = items[0]
-            meta = item['meta']
-
-            return {
-                'task_id': item['name'],
-                'timestamp': meta['timestamp'],
-                'result': [
-                    self.app.AsyncResult(task)
-                    for task in self.decode(meta['result'])
-                ],
-            }
+        if self._in_girder:
+            return self._restore_group_girder(group_id)
+        else:
+            return self._restore_group_girder_client(group_id)
