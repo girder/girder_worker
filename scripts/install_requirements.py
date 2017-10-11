1ceb2c75d6d7bb61007ec86be38d9aa48881dc19
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-10-11 13:35:47 -0400

    Remove requirements unpinning in setup.py

--- scripts/install_requirements.py
+++ scripts/install_requirements.py
@@ -7,24 +7,6 @@ import pip
 import sys
 
 
-def installReqs(file):
-    if not os.path.exists(file):
-        return
-
-    print('\033[32m*** Installing: %s\033[0m' % file)
-    if pip.main(['install', '-U', '-r', file]) != 0:
-        print('\033[1;91m*** Error installing %s, stopping.\033[0m' % file,
-              file=sys.stderr)
-        sys.exit(1)
-
-
-def installFromDir(path, dev):
-    installReqs(os.path.join(path, 'requirements.txt'))
-
-    if dev:
-        installReqs(os.path.join(path, 'requirements-dev.txt'))
-
-
 def main(args):
     basePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,22 +15,32 @@ def main(args):
     config = ConfigParser()
     config.read([os.path.join(basePath, 'girder_worker', f) for f in files])
     os.chdir(basePath)
-    isDevMode = args.mode in ('dev', 'devel', 'development')
     plugins = os.environ.get(
         'GIRDER_WORKER_PLUGINS_ENABLED',
         config.get('girder_worker', 'plugins_enabled'))
     plugins = [p.strip() for p in plugins.split(',') if p.strip()]
 
-    # Install core requirements files
-    installFromDir(basePath, isDevMode)
+    installArgs = ['requirements.in']
+    if args.mode in ('dev', 'devel', 'development'):
+        installArgs.append('requirements-dev.in')
 
     # Install plugins requirements files
-    pluginsDir = os.path.join(basePath, 'girder_worker', 'plugins')
+    pluginsDir = os.path.join('girder_worker', 'plugins')
     for path in os.listdir(pluginsDir):
         if args.all or path in plugins:
-            pluginPath = os.path.join(pluginsDir, path)
-            if os.path.isdir(pluginPath):
-                installFromDir(pluginPath, isDevMode)
+            pluginReqs = os.path.join(pluginsDir, path, 'requirements.txt')
+            if os.path.exists(os.path.join(basePath, pluginReqs)):
+                installArgs.append(pluginReqs)
+
+    pipArgs = ['install'] + [
+        os.path.join('-r' + basePath, req) for req in installArgs
+    ]
+
+    print('\033[32m*** Installing: %s\033[0m' % ' '.join(installArgs))
+    if pip.main(pipArgs) != 0:
+        print('\033[1;91m*** Error installing %s, stopping.\033[0m' % file,
+              file=sys.stderr)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
