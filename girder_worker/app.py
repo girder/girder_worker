26541054858397a079832d22a3f8a1af7e570d83
Christopher Kotfila <chris.kotfila@kitware.com>
2017-11-29 13:55:50 -0500

    Fixes typo with _maybe_model_repr utility function

--- girder_worker/app.py
+++ girder_worker/app.py
@@ -182,7 +182,7 @@ class Task(celery.Task):
 
 
 def _maybe_model_repr(obj):
-    if hasattr(obj, '__repr_model_') and hasattr(obj._repr_model_, '__call__'):
+    if hasattr(obj, '_repr_model_') and hasattr(obj._repr_model_, '__call__'):
         return obj._repr_model_()
     return obj
 
