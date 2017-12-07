1a1eec94c8b574b4926c46b39d8ea84d90dd17cd
Christopher Kotfila <chris.kotfila@kitware.com>
2017-12-07 11:07:00 -0500

    Refactor Celery config into celeryconfig.py
    This also adds support for GIRDER_WORKER_BROKER and
    GIRDER_WORKER_BACKEND environment variables which take precedence over
    variables defined in worker.local.cfg and worker.dist.cfg

---
+++ girder_worker/celeryconfig.py
@@ -0,0 +1,16 @@
+import os
+from six.moves import configparser
+import girder_worker
+
+accept_content = ['json', 'pickle', 'yaml', 'girder_io']
+task_serializer = 'girder_io'
+result_serializer = 'girder_io'
+
+broker_url = os.environ.get('GIRDER_WORKER_BROKER', None) or \
+    girder_worker.config.get('celery', 'broker')
+
+try:
+    result_backend = os.environ.get('GIRDER_WORKER_BACKEND', None) or \
+        girder_worker.config.get('celery', 'backend')
+except configparser.NoOptionError:
+    result_backend = broker_url
