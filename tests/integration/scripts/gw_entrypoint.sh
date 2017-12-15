53f5bf91ff03fe500b794dd48d1630ad9a2f26ef
Christopher Kotfila <chris.kotfila@kitware.com>
2017-12-15 15:15:06 -0500

    First pass at running docker integration tests locally

--- tests/integration/scripts/gw_entrypoint.sh
+++ tests/integration/scripts/gw_entrypoint.sh
@@ -16,6 +16,17 @@ if [ -e /girder_worker/tests/integration/common_tasks/setup.py ]; then
     pip install -e /girder_worker/tests/integration/common_tasks/
 fi
 
+# Hack to make sure docker.sock has a real group, and that the
+# 'worker' user is apart of that group.
+if [ -e /var/run/docker.sock ]; then
+    if [ $(stat -c %G /var/run/docker.sock) == 'UNKNOWN' ]; then
+        groupadd -g $(stat -c %g /var/run/docker.sock) dockermock
+        usermod -aG dockermock worker
+    else
+        usermod -aG $(stat -c %G /var/run/docker.sock) worker
+    fi
+fi
+
 mkdir tmp
 chmod 777 tmp
 
