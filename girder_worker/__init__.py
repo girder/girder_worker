3cb520a8f737af5be0b748eafccf40faade0d042
Zach Mullen <zach.mullen@kitware.com>
2018-01-31 10:07:28 -0500

    Bump version to 0.4.1

--- girder_worker/__init__.py
+++ girder_worker/__init__.py
@@ -3,7 +3,7 @@ import os
 from . import log_utils
 from six.moves.configparser import SafeConfigParser
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __license__ = 'Apache 2.0'
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
