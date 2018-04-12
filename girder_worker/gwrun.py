3fa96c9816af6284573bdfbc510c6967c2a00695
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-04-12 15:16:36 -0400

    Don't group tasks by gw plugin

--- girder_worker/gwrun.py
+++ girder_worker/gwrun.py
@@ -3,44 +3,23 @@ import click
 from .entrypoint import get_extensions, get_extension_tasks, import_all_includes
 
 
-def _get_task_command(extension, name):
+def _iterate_tasks():
     import_all_includes()
-    for task in get_extension_tasks(extension).values():
-        if name == task.main.name:
-            return task.main
+    for extension in get_extensions():
+        if extension not in ('core', 'docker'):
+            for task in get_extension_tasks(extension).values():
+                yield task.main.name, task.main
 
 
 class GWCommand(click.MultiCommand):
     def list_commands(self, ctx):
-        import_all_includes()
-        for extension in get_extensions():
-            if extension not in ('core', 'docker'):
-                yield extension
+        for name, task in _iterate_tasks():
+            yield name
 
     def get_command(self, ctx, name):
-        if name not in self.list_commands(ctx):
-            return None
-        return GWExtensionCommand(name)
-
-
-class GWExtensionCommand(click.MultiCommand):
-    def __init__(self, extension):
-        self._extension = extension
-        super(GWExtensionCommand, self).__init__()
-
-    def list_commands(self, ctx):
-        import_all_includes()
-        for task in get_extension_tasks(self._extension).values():
-            yield task.main.name
-
-    def get_command(self, ctx, name):
-        import_all_includes()
-        if name not in self.list_commands(ctx):
-            return None
-
-        for task in get_extension_tasks(self._extension).values():
-            if name == task.main.name:
-                return task.main
+        for name_, task in _iterate_tasks():
+            if name_ == name:
+                return task
 
 
 @click.command(cls=GWCommand)
