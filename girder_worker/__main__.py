6869fdce36de0e721fb53cf43ac3a9bc45b6d2f0
David Manthey <david.manthey@kitware.com>
2022-11-17 08:24:53 -0500

    Update images used in testing on circle-ci.

--- girder_worker/__main__.py
+++ girder_worker/__main__.py
@@ -4,7 +4,7 @@ from .app import app
 
 
 def main():
-    app.worker_main(argv=['worker'] + sys.argv[1:])
+    app.worker_main(argv=['worker'] + sys.argv[1:] if 'worker' not in sys.argv else sys.argv)
 
 
 if __name__ == '__main__':
